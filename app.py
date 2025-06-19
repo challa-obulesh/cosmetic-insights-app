698ba3c5fdb9cd5c02a1b79af2c83d40990b632b
challa chinnaobulesh <challaobulesh3@gmail.com>
2025-06-19 16:22:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/dashboard1')
+def dashboard1():
+    return render_template('dashboard1.html')
+
+@app.route('/dashboard2')
+def dashboard2():
+    return render_template('dashboard2.html')
+
+@app.route('/dashboard3')
+def dashboard3():
+    return render_template('dashboard3.html')
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    if request.method == 'POST':
+        name = request.form['name']
+        comments = request.form['comments']
+        print(f"Feedback received from {name}: {comments}")
+        return redirect('/')
+    return render_template('feedback.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
